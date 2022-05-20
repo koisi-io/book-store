139206e9a05d8de716487d07181ee542d4f8bd37
koisi <koisi.poi@gmail.com>
2022-05-20 17:32:16 +0800

    format SpimLock

--- src/core/SpinLock.hpp
+++ src/core/SpinLock.hpp
@@ -1,5 +1,4 @@
 // Copyright (c) 2020 by Chrono
-
 #ifndef _SPIN_LOCK_HPP
 #define _SPIN_LOCK_HPP
 
@@ -8,71 +7,52 @@
 BEGIN_NAMESPACE(cpp_study)
 
 // atomic spinlock with TAS
-class SpinLock final
-{
-public:
+class SpinLock final{
+  public:
     using this_type   = SpinLock;
     using atomic_type = std::atomic_flag;
+  private:
+    atomic_type m_lock {false};
 
-public:
+  public:
     SpinLock() = default;
    ~SpinLock() = default;
-
+    // 禁止拷贝构造
     SpinLock(const this_type&) = delete;
     SpinLock& operator=(const this_type&) = delete;
-public:
-    void lock() noexcept
-    {
-        for(;;) {
-            if (!m_lock.test_and_set()) {
-                return;
-            }
 
+    void lock() noexcept {
+        for(;;) {
+            if (!m_lock.test_and_set()) { return; }
             std::this_thread::yield();
         }
     }
-
-    bool try_lock() noexcept
-    {
-        return !m_lock.test_and_set();
-    }
-
-    void unlock() noexcept
-    {
-        m_lock.clear();
-    }
-private:
-    atomic_type m_lock {false};
+    bool try_lock() noexcept { return !m_lock.test_and_set(); }
+    void unlock()   noexcept { m_lock.clear(); }
 };
 
 // RAII for lock
 // you can change it to a template class
-class SpinLockGuard final
-{
-public:
+class SpinLockGuard final {
+  public:
     using this_type      = SpinLockGuard;
     using spin_lock_type = SpinLock;
-public:
-    SpinLockGuard(spin_lock_type& lock) noexcept
-        : m_lock(lock)
-    {
+  private:
+    spin_lock_type& m_lock;
+
+  public:
+    explicit SpinLockGuard(spin_lock_type& lock) noexcept : m_lock(lock){
         m_lock.lock();
     }
-
-   ~SpinLockGuard() noexcept
-   {
+   ~SpinLockGuard() noexcept {
        m_lock.unlock();
    }
 
 public:
-    SpinLockGuard(const this_type&) = delete;
+    SpinLockGuard(const this_type&)            = delete;
     SpinLockGuard& operator=(const this_type&) = delete;
-private:
-    spin_lock_type& m_lock;
 };
-
 
 END_NAMESPACE(cpp_study)
 
 #endif  //_SPIN_LOCK_HPP
-
