663d3256270fdf8ae04de3d18f28b0810ed34677
koisi <koisi.poi@gmail.com>
2022-05-24 11:37:08 +0800

    note get configuration from lua script

--- src/conf.lua
+++ src/conf.lua
@@ -5,26 +5,17 @@
 -- and more lua pragram
 
 config = {
-
     -- should be same as client
     -- you could change it to ipc
     zmq_ipc_addr = "tcp://127.0.0.1:5555",
-
     -- see http_study's lua code
     http_addr = "http://localhost/cpp_study?token=cpp@2020",
-
     time_interval = 5,  -- seconds
-
     keyword = "super",
-
     sold_criteria = 100,
-
     revenue_criteria = 1000,
-
     best_n  = 3,
-
     max_buf_size = 4 * 1024,
-
 }
 
 -- more config
@@ -33,5 +24,4 @@ others = {
 }
 
 -- debug: luajit conf.lua
-
---print(config.http_addr)
+-- print(config.http_addr)
--- src/core/Config.hpp
+++ src/core/Config.hpp
@@ -15,38 +15,35 @@ extern "C" {
 
 BEGIN_NAMESPACE(cpp_study)
 
-class Config final
-{
-public:
+class Config final{
+  public:
     using vm_type           = std::shared_ptr<lua_State>;
     using value_type        = luabridge::LuaRef;
-
     using string_type       = std::string;
     using string_view_type  = const std::string&;
     using regex_type        = std::regex;
     using match_type        = std::smatch;
-public:
-    Config() noexcept
-    {
-        assert(m_vm);
 
+  private:
+    // 这里都是 对象实例， 而不是 指针
+    vm_type             m_vm     {luaL_newstate(), lua_close};
+    // 按照格式: "config.zmq_ipc_addr" 来获取参数
+    const   regex_type  m_reg    {R"(^(\w+)\.(\w+)$)"};
+    mutable match_type  m_what;
+public:
+    Config() noexcept{
+        assert(m_vm);
         luaL_openlibs(m_vm.get());
     }
-
    ~Config() = default;
-public:
-    void load(string_view_type filename) const
-    {
-        auto status = luaL_dofile(m_vm.get(), filename.c_str());
 
-        if (status != 0) {
-            throw std::runtime_error("failed to parse config");
-        }
+    void load(string_view_type filename) const {
+        auto status = luaL_dofile(m_vm.get(), filename.c_str());
+        if (status != 0) { throw std::runtime_error("failed to parse config"); }
     }
 
     template<typename T>
-    T get(string_view_type key) const
-    {
+    T get(string_view_type key) const {
         if (!std::regex_match(key, m_what, m_reg)) {
             throw std::runtime_error("config key error");
         }
@@ -55,17 +52,9 @@ public:
         auto w2 = m_what[2].str();
 
         using namespace luabridge;
-
-        auto v = getGlobal(
-                    m_vm.get(), w1.c_str());
-
+        auto v = getGlobal(m_vm.get(), w1.c_str());
         return LuaRef_cast<T>(v[w2]);
     }
-private:
-    vm_type     m_vm {luaL_newstate(), lua_close};
-
-    const regex_type    m_reg {R"(^(\w+)\.(\w+)$)"};
-    mutable match_type  m_what;
 };
 
 END_NAMESPACE(cpp_study)
