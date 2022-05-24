523ebd10e65b56c4b3f3916236c32f82371bd84f
koisi <koisi.poi@gmail.com>
2022-05-24 10:48:43 +0800

    note izmp

--- src/core/izmp.hpp
+++ src/core/izmp.hpp
@@ -3,10 +3,8 @@
 #ifndef _IZMQ_HPP
 #define _IZMQ_HPP
 
-#include "cpplang.hpp"
-
-// /usr/include/zmq.hpp
 #include <zmq.hpp>
+#include "cpplang.hpp"
 
 BEGIN_NAMESPACE(cpp_study)
 
@@ -15,28 +13,23 @@ using zmq_socket_type   = zmq::socket_t;
 using zmq_message_type  = zmq::message_t;
 
 template<int thread_num = 1>
-class ZmqContext final
-{
+class ZmqContext final {
 #if 0
-public:
+  public:
     using zmq_context_type  = zmq::context_t;
     using zmq_socket_type   = zmq::socket_t;
     using zmq_message_type  = zmq::message_t;
 #endif
-public:
+  public:
     ZmqContext() = default;
    ~ZmqContext() = default;
-public:
-    static
-    zmq_context_type& context()
-    {
+  public:
+    static zmq_context_type& context() {
         static zmq_context_type ctx(thread_num);
         return ctx;
     }
-public:
-    static
-    zmq_socket_type recv_sock(int hwm = 1000, int linger = 10)
-    {
+  public:
+    static zmq_socket_type recv_sock(int hwm = 1000, int linger = 10){
         zmq_socket_type sock(context(), ZMQ_PULL);
 
         sock.setsockopt(ZMQ_RCVHWM, hwm);
@@ -45,9 +38,7 @@ public:
         return sock;
     }
 
-    static
-    zmq_socket_type send_sock(int hwm = 1000, int linger = 10)
-    {
+    static zmq_socket_type send_sock(int hwm = 1000, int linger = 10){
         zmq_socket_type sock(context(), ZMQ_PUSH);
 
         sock.setsockopt(ZMQ_SNDHWM, hwm);
@@ -60,4 +51,3 @@ public:
 END_NAMESPACE(cpp_study)
 
 #endif  //_ZMQ_HPP
-
