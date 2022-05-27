b053e33a09059c425412b747c91365aaf025eb74
koisi <koisi.poi@gmail.com>
2022-05-27 15:56:23 +0800

    Revert "Note client.cpp"
    This reverts commit ebb85f50880ce80d7dd3a84bb30b2daf16bd88d9.

--- src/main/client.cpp
+++ src/main/client.cpp
@@ -1,30 +1,19 @@
 // Copyright (c) 2020 by Chrono
-//
-// g++ client.cpp -std=c++14 -I../common -I../common/include -lzmq -lpthread -o c.out;./c.out
-// g++ client.cpp -std=c++14 -I../common -I../common/include -lzmq -lpthread -g -O0 -o c.out
-// g++ client.cpp -std=c++14 -I../common -I../common/include -lzmq -lpthread -g -O0 -o c.out;./c.out
-
-//#include <iostream>
 
 #include "cpplang.hpp"
 #include "SalesData.hpp"
 #include "izmp.hpp"
-
-// you should put json.hpp in ../common
 #include "json.hpp"
 
 USING_NAMESPACE(std);
 USING_NAMESPACE(cpp_study);
 
 static
-auto debug_print = [](auto& b)
-{
+auto debug_print = [](auto& b){
     using json_t = nlohmann::json;
-
     json_t j;
-
-    j["id"] = b.id();
-    j["sold"] = b.sold();
+    j["id"]      = b.id();
+    j["sold"]    = b.sold();
     j["revenue"] = b.revenue();
     //j["average"] = b.average();
 
@@ -33,54 +22,27 @@ auto debug_print = [](auto& b)
 
 // sales data
 static
-auto make_sales = [](const auto& id, auto s, auto r)
-//-> msgpack::sbuffer
-{
-    return SalesData(id, s, r).pack();
-
-#if 0
-    SalesData book(id);
-
-    book.inc_sold(s);
-    book.inc_revenue(r);
-
-    debug_print(book);
-
-    auto buf = book.pack();
-    cout << buf.size() << endl;
-
-    //SalesData book2 {buf};
-    //assert(book.id() == book2.id());
-    //debug_print(book2);
-
-    return buf;
-#endif
+auto make_sales = [](const auto& id, auto sold, auto revenue){
+    return SalesData(id, sold, revenue).pack();
 };
 
 // zmq send
 static
-auto send_sales = [](const auto& addr, const auto& buf)
-{
+auto send_sales = [](const auto& addr, const auto& buf){
     using zmq_ctx = ZmqContext<1>;
-
     auto sock = zmq_ctx::send_sock();
-
     sock.connect(addr);
-    assert(sock.connected());
+    assert(sock);
 
-    auto len = sock.send(buf.data(), buf.size());
+    zmq::const_buffer zmq_buf((void*)buf.data(),buf.size());
+    auto len = sock.send(zmq_buf);
     assert(len == buf.size());
-
-    cout << "send len = " << len << endl;
+    if(len == buf.size()){ cout << "yes" << endl;  }
 };
 
 int main()
-try
-{
+try {
     cout << "hello cpp_study client" << endl;
-
-    //auto buf = make_sales("001", 10, 100);
-    //send_sales("tcp://127.0.0.1:5555", buf);
 
     send_sales("tcp://127.0.0.1:5555",
              make_sales("001", 10, 100));
@@ -88,10 +50,8 @@ try
     std::this_thread::sleep_for(100ms);
 
     send_sales("tcp://127.0.0.1:5555",
-             make_sales("002", 20, 200));
-
+               make_sales("002", 20, 200));
 }
-catch(std::exception& e)
-{
+catch(std::exception& e){
     std::cerr << e.what() << std::endl;
 }
